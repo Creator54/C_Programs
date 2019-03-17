d0da63d6a3131ddfc5a951e8223d822aa42e58ee
Creator54 <34543609+Creator54@users.noreply.github.com>
2019-03-17 13:25:57 +0530

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+//Basic Hello World Program//
+#include<stdio.h>
+//This Header file contains the instructions for the printf and scanf functions//
+void main()
+//This indicates that the function being made returns no value//
+//"{}" This indicates the beginning and ending of the code//
+{
+printf("Hello World");
+}
