02cb86ba6d455f81ec226c708c80fbc5be256d78
TANNU KUMARI <tannuchoudhary10@gmail.com>
2019-10-02 12:08:46 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,19 +1,64 @@
 //Basic Arithmetic Operations using C//
+
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 void main()
 {
-int i,j,k;
-//specifing two integer variables as i and j//
-printf("Basic Calcy\n");
-printf("Enter the number :\t");
-scanf("%d",&i);
-printf("Input operation +,-,*,/,!\n");
-scanf("%d",&j);
-printf("%d",j);
-if (j=='+')
-{
-printf("Enter 2nd Number :\t");
-scanf("%d",&k);
-printf("Sum =\ti+k");
-}
+  int num1, num2, cal;
+  int oprtr;
+   printf("\n1.Addition \n2.Subtraction \n3.Multiplication \n4.Division \n5.Modulus \n6.power \n7.Exit\n");
+  while(1)
+  {
+     printf("Choose your operation : ");
+     scanf("%d", &oprtr);
+    switch(oprtr)
+    {
+     case 1 :   printf("Enter your first number : ");
+                scanf("%d", &num1);
+                printf("Enter your second number : ");
+                scanf("%d", &num2);
+                cal = num1 + num2;
+                printf("Sum is %d\n", cal);
+                break;
+    case 2 :    printf("Enter your first number : ");
+                scanf("%d", &num1);
+                printf("Enter your second number : ");
+                scanf("%d", &num2);
+                cal = num1 - num2;
+                printf("Difference is %d\n", cal);
+                break;
+    case 3 :    printf("Enter your first number : ");
+                scanf("%d", &num1);
+                printf("Enter your second number : ");
+                scanf("%d", &num2);
+                cal = num1 * num2;
+                printf("Product is %d\n", cal);
+                break;
+    case 4 :    printf("Enter your dividend : ");
+                scanf("%d", &num1);
+                printf("Enter your divisor : ");
+                scanf("%d", &num2);
+                cal = num1 / num2;
+                printf("Quotient is %d\n", cal);
+                break;
+    case 5  :   printf("Enter your dividend : ");
+                scanf("%d", &num1);
+                printf("Enter your divisor : ");
+                scanf("%d", &num2);
+                cal = num1 % num2;
+                printf("Remainder is %d\n", cal);
+                break;
+    case 6 :    printf("Enter base : ");
+                scanf("%d", &num1);
+                printf("Enter power : ");
+                scanf("%d", &num2);
+                cal = pow(num1, num2);
+                printf("Result is %d\n", cal);
+                break;
+    case 7 : exit(0);
+    default : printf("Entered choice is not correct.\n");
+              break;
+    }
+  }  
 }
