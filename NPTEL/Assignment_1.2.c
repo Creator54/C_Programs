e616aeebfbf54726c6a774376bba07bb4fcf0bc9
Saroj Kumar Mahato <sarojkumarmahato32@gmail.com>
2019-10-17 12:27:37 +0530

    Added Assignment_1.2

---
+++ NPTEL/Assignment_1.2.c
@@ -0,0 +1,24 @@
+/*
+You are given two integers, say M and N.
+
+You must check whether M is an exact multiple of N, without using
+loops.
+You have to output 0 if M is not a multiple of N.
+You have to output M/N if M is a multiple of N.
+
+Input-----
+Two integers, say M and N.
+
+Output------
+You have to output 0 if M is not a multiple of N.
+You have to output M/N if M is a multiple of N.
+*/
+#include<stdio.h>
+void main(){
+  int m,n;
+  scanf("%d%d",&m,&n);
+  if((m%n)==0)
+    printf("%d",m/n);
+  else
+    printf("0");
+}
