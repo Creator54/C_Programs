0c8059dbf2b82f33a42600281e36ab0a110e458e
SAROJ KUMAR MAHATO <34543609+Creator54@users.noreply.github.com>
2019-12-05 19:05:27 +0530

    bug still exists

--- Sorting/selection_sort.c
+++ Sorting/selection_sort.c
@@ -21,7 +21,7 @@ int main(){
                 s_index=j;
             }
         }
-        if(b[i+1]<b[i]){
+        if(b[i]>b[s_index]){
             b[s_index]=b[i];
             b[i]=smallest;
         }
@@ -32,3 +32,95 @@ int main(){
     }
     return 0;
 }
+
+/*
+
+Output1:
+
+Enter array size :                                                                                                                                                                       
+5                                                                                                                                                                                        
+Enter array elements :                                                                                                                                                                   
+                                                                                                                                                                                         
+5 4 3 2 1                                                                                                                                                                                
+                                                                                                                                                                                         
+After 1 iteration                                                                                                                                                                        
+1 4 3 2 5                                                                                                                                                                                
+After 2 iteration                                                                                                                                                                        
+1 2 3 4 5                                                                                                                                                                                
+After 3 iteration                                                                                                                                                                        
+1 2 3 4 5                                                                                                                                                                                
+After 4 iteration                                                                                                                                                                        
+1 2 3 4 5                                                                                                                                                                                
+                                                                                                                                                                                         
+...Program finished with exit code 0 
+
+
+Output2:
+
+Enter array size :                                                                                                                                                                      
+5                                                                                                                                                                                       
+Enter array elements :                                                                                                                                                                  
+                                                                                                                                                                                        
+12 4 86 9 0                                                                                                                                                                             
+                                                                                                                                                                                        
+After 1 iteration                                                                                                                                                                       
+0 4 86 9 12                                                                                                                                                                             
+After 2 iteration                                                                                                                                                                       
+0 4 86 9 12                                                                                                                                                                             
+After 3 iteration                                                                                                                                                                       
+0 4 12 9 86                                                                                                                                                                             
+After 4 iteration                                                                                                                                                                       
+0 4 12 9 86                                                                                                                                                                             
+                                                                                                                                                                                        
+...Program finished with exit code 0 
+
+
+Output3 (WRONG OUTPUT)
+
+Enter array size :                                                                                                                                                                      
+5                                                                                                                                                                                       
+Enter array elements :                                                                                                                                                                  
+                                                                                                                                                                                        
+2312456 78 456 243 778                                                                                                                                                                  
+                                                                                                                                                                                        
+After 1 iteration                                                                                                                                                                       
+778 78 456 243 2312456                                                                                                                                                                  
+After 2 iteration                                                                                                                                                                       
+778 78 456 243 2312456                                                                                                                                                                  
+After 3 iteration                                                                                                                                                                       
+778 78 243 456 2312456                                                                                                                                                                  
+After 4 iteration                                                                                                                                                                       
+778 78 243 456 2312456                                                                                                                                                                  
+                                                                                                                                                                                        
+...Program finished with exit code 0
+
+
+Output4 (WRONG OUTPUT):
+
+10                                                                                                                                                                                      
+Enter array elements :                                                                                                                                                                  
+                                                                                                                                                                                        
+7669 5567 21 7 0 645 8 35 2 67                                                                                                                                                          
+                                                                                                                                                                                        
+After 1 iteration                                                                                                                                                                       
+67 5567 21 7 0 645 8 35 2 7669                                                                                                                                                          
+After 2 iteration                                                                                                                                                                       
+67 2 21 7 0 645 8 35 5567 7669                                                                                                                                                          
+After 3 iteration                                                                                                                                                                       
+67 2 8 7 0 645 21 35 5567 7669                                                                                                                                                          
+After 4 iteration                                                                                                                                                                       
+67 2 8 0 7 645 21 35 5567 7669                                                                                                                                                          
+After 5 iteration                                                                                                                                                                       
+67 2 8 0 7 645 21 35 5567 7669                                                                                                                                                          
+After 6 iteration                                                                                                                                                                       
+67 2 8 0 7 35 21 645 5567 7669                                                                                                                                                          
+After 7 iteration                                                                                                                                                                       
+67 2 8 0 7 35 21 645 5567 7669                                                                                                                                                          
+After 8 iteration                                                                                                                                                                       
+67 2 8 0 7 35 21 645 5567 7669                                                                                                                                                          
+After 9 iteration                                                                                                                                                                       
+67 2 8 0 7 35 21 5567 35 7669                                                                                                                                                           
+                                                                                                                                                                                        
+...Program finished with exit code 0    
+
+*/
